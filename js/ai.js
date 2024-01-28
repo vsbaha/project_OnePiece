ba239707458e789e73e4e1e3fa07a43455d4e8a5
vsbaha <azimkanovbaktiar@gmail.com>
2024-01-28 10:19:28 +0600

    DZ7

--- js/ai.js
+++ js/ai.js
@@ -1,22 +1,22 @@
-const OpenAI = require("openai");
-const openai = new OpenAI({
-    apiKey: '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',
-});
-
-exports.chatReq = async (req, res) => {
-    try {
-        const message = "Which is the capital of Albania?";
-        const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: message }],
-            temperature: 0,
-            max_tokens: 1000,
-        });
-        res.status(200).json(response);
-    } catch (err) {
-        res.status(500).json(err.message);
-    }
-};
+// const OpenAI = require("openai");
+// const openai = new OpenAI({
+//     apiKey: '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',
+// });
+//
+// exports.chatReq = async (req, res) => {
+//     try {
+//         const message = "Which is the capital of Albania?";
+//         const response = await openai.chat.completions.create({
+//             model: "gpt-3.5-turbo",
+//             messages: [{ role: "user", content: message }],
+//             temperature: 0,
+//             max_tokens: 1000,
+//         });
+//         res.status(200).json(response);
+//     } catch (err) {
+//         res.status(500).json(err.message);
+//     }
+// };
 
 const chatContainer = document.querySelector('#chat-container');
 const userInput = document.querySelector('#user-input');
